6edf8d82e61c9cd7eb24668114378e10f0a63ea2
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-12-14 22:24:07 +0100

    Add new backend project

---
+++ Backend/Pollenalarm.Backend.AspNet/Controllers/PollenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pollenalarm.Backend.AspNet.Controllers
+{
+    [RoutePrefix("api/pollen")]
+    public class PollenController : ApiController
+    {
+        [HttpGet]
+        //[ResponseType(typeof(List<Pollen>))]
+        public IHttpActionResult GetAllPollen()
+        {
+            return Ok("Hallo Pollen");
+        }
+    }
+}
---
+++ Backend/Pollenalarm.Backend.AspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pollenalarm.Backend.AspNet.WebApiApplication" Language="C#" %>
