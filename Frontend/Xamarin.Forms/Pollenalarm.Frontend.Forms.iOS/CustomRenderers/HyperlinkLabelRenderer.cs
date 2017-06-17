76c98208a484c6612b4261804071a13340e3a541
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-06-17 22:41:39 +0200

    Replace 'About' button with Hyperlink label

---
+++ Frontend/Xamarin.Forms/Pollenalarm.Frontend.Forms.iOS/CustomRenderers/HyperlinkLabelRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Pollenalarm.Frontend.Forms.CustomControls;
+using Pollenalarm.Frontend.Forms.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HyperlinkLabel), typeof(HyperlinkLabelRenderer))]
+namespace Pollenalarm.Frontend.Forms.iOS.CustomRenderers
+{
+    public class HyperlinkLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                // Set UILabel underlining
+                var text = (NSMutableAttributedString)Control.AttributedText;
+                text.AddAttribute(UIStringAttributeKey.UnderlineStyle, NSNumber.FromInt32((int)NSUnderlineStyle.Single), new NSRange(0, text.Length));
+            }
+        }
+    }
+}
