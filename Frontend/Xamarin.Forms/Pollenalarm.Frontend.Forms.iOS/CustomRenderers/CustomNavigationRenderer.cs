eb089d7036fa3103795bb4c1231a93c4175f7907
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2017-09-28 19:34:06 +0200

    Package updates

--- Frontend/Xamarin.Forms/Pollenalarm.Frontend.Forms.iOS/CustomRenderers/CustomNavigationRenderer.cs
+++ Frontend/Xamarin.Forms/Pollenalarm.Frontend.Forms.iOS/CustomRenderers/CustomNavigationRenderer.cs
@@ -6,39 +6,39 @@ using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(NavigationPage), typeof(CustomNavigationRenderer))]
+//[assembly: ExportRenderer(typeof(NavigationPage), typeof(CustomNavigationRenderer))]
 namespace Pollenalarm.Frontend.Forms.iOS.CustomRenderers
 {
-	/// <summary>
-	/// Custom navigation renderer to move the 'Settings' button to the left on iOS
-	/// </summary>
-	public class CustomNavigationRenderer : NavigationRenderer
-	{
-		public override void PushViewController(UIKit.UIViewController viewController, bool animated)
-		{
-			base.PushViewController(viewController, animated);
+    /// <summary>
+    /// Custom navigation renderer to move the 'Settings' button to the left on iOS
+    /// </summary>
+    public class CustomNavigationRenderer : NavigationRenderer
+    {
+        public override void PushViewController(UIKit.UIViewController viewController, bool animated)
+        {
+            base.PushViewController(viewController, animated);
 
-			var rightList = new List<UIBarButtonItem>();
-			var leftList = new List<UIBarButtonItem>();
+            var rightList = new List<UIBarButtonItem>();
+            var leftList = new List<UIBarButtonItem>();
 
-			foreach (var item in TopViewController.NavigationItem.RightBarButtonItems)
-			{
-				// Get original Xamarin.Forms's ToolBar item through reflection
-				var controller = (ToolbarItem)item.GetType().GetProperty("Controller",
-				   BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).GetValue(item);
+            foreach (var item in TopViewController.NavigationItem.RightBarButtonItems)
+            {
+                // Get original Xamarin.Forms's ToolBar item through reflection
+                var controller = (ToolbarItem)item.GetType().GetProperty("Controller",
+                   BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).GetValue(item);
 
-				// Check if it is a settings icon by comparing its AutomationId
-				if (controller.AutomationId != null && controller.AutomationId.ToLower().Contains("settings"))
-					leftList.Add(item);
-				else
-					rightList.Add(item);
-			}
+                // Check if it is a settings icon by comparing its AutomationId
+                if (controller.AutomationId != null && controller.AutomationId.ToLower().Contains("settings"))
+                    leftList.Add(item);
+                else
+                    rightList.Add(item);
+            }
 
-			if (leftList.Any())
-				TopViewController.NavigationItem.LeftBarButtonItems = leftList.ToArray();
+            if (leftList.Any())
+                TopViewController.NavigationItem.LeftBarButtonItems = leftList.ToArray();
 
-			if (rightList.Any())
-				TopViewController.NavigationItem.RightBarButtonItems = rightList.ToArray();
-		}
-	}
+            if (rightList.Any())
+                TopViewController.NavigationItem.RightBarButtonItems = rightList.ToArray();
+        }
+    }
 }
